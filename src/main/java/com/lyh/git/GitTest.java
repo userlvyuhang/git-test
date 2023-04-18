62411602bf0fed9fe2167149f9d7968b47976419
userlvyuhang <127387148+userlvyuhang@users.noreply.github.com>
2023-04-18 19:45:00 +0800

    pull my

--- src/main/java/com/lyh/git/GitTest.java
+++ src/main/java/com/lyh/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("i am 'hot-fix' ");
         System.out.println("master test ");
         System.out.println("hot-fix test");
+        System.out.println("pull test");
     }
 }
